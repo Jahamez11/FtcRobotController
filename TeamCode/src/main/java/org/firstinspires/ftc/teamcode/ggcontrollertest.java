89600db5b74f61ffa34c128176414a43153653cb
Jahamez11 <145066370+Jahamez11@users.noreply.github.com>
2023-10-28 13:22:46 -0700

    Competition Code Saturday Oct-28

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ggcontrollertest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ggcontrollertest.java
@@ -1,36 +1,44 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.view.animation.GridLayoutAnimationController;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "ggcontrollertest", group = "LinearOpMode")
 public class ggcontrollertest extends LinearOpMode {
 
-    private DcMotor left_back_driveAsDcMotor;
-    private DcMotor right_back_driveAsDcMotor;
+    private DcMotor LEFT_DRIVEAsDcMotor;
+    private DcMotor RIGHT_DRIVEAsDcMotor;
+    private DcMotor ARM_ROTATIONDcMotor;
+    private DcMotor ARM_EXTENSIONDcMotor;
 
-    /**
-     * This function is executed when this Op Mode is selected from the Driver Station.
-     */
+    private CRServo PLANE_LAUNCHER;
+
+
+    // This function is executed when this Op Mode is selected from the Driver Station
     @Override
     public void runOpMode() {
         float Axial;
         float Yaw;
         float Speed_multiplier;
-        float min_speed = (float)0.25;
+        float min_speed = (float) 0.25;
         float robot_speed;
         float leftBackPower;
         float rightBackPower;
         float max;
 
-        left_back_driveAsDcMotor = hardwareMap.get(DcMotor.class, "left_back_drive");
-        right_back_driveAsDcMotor = hardwareMap.get(DcMotor.class, "right_back_drive");
-        left_back_driveAsDcMotor.setDirection(DcMotor.Direction.FORWARD);
+        LEFT_DRIVEAsDcMotor = hardwareMap.get(DcMotor.class, "LEFT_DRIVE");
+        RIGHT_DRIVEAsDcMotor = hardwareMap.get(DcMotor.class, "RIGHT_DRIVE");
+        ARM_ROTATIONDcMotor = hardwareMap.get(DcMotor.class, "ARM_ROTATION");
+        LEFT_DRIVEAsDcMotor.setDirection(DcMotor.Direction.FORWARD);
+        ARM_ROTATIONDcMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         //rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        right_back_driveAsDcMotor.setDirection(DcMotor.Direction.REVERSE);
-
+        RIGHT_DRIVEAsDcMotor.setDirection(DcMotor.Direction.REVERSE);
+        //armExtension.setDirection(DCMotor.Direction.Reverse);
         // Put initialization blocks here.
         waitForStart();
         if (opModeIsActive()) {
@@ -38,12 +46,13 @@ public class ggcontrollertest extends LinearOpMode {
             while (opModeIsActive()) {
                 // Put loop blocks here.
                 telemetry.update();
+
                 // X = yaw. Y = axial
                 Axial = gamepad1.left_stick_y;
                 Yaw = gamepad1.left_stick_x;
                 //Increase Speed based on trigger;
                 Speed_multiplier = gamepad1.right_trigger;
-                robot_speed = min_speed + ((1-min_speed) * Speed_multiplier);
+                robot_speed = min_speed + ((1 - min_speed) * Speed_multiplier);
                 leftBackPower = Axial + Yaw;
                 rightBackPower = Axial - Yaw;
                 if (Math.abs(leftBackPower) > Math.abs(rightBackPower)) {
@@ -55,12 +64,11 @@ public class ggcontrollertest extends LinearOpMode {
                     leftBackPower = leftBackPower / max;
                     rightBackPower = rightBackPower / max;
                 }
-                leftBackPower *=robot_speed;
-                rightBackPower *=robot_speed;
-                left_back_driveAsDcMotor.setPower(leftBackPower);
-                right_back_driveAsDcMotor.setPower(rightBackPower);
+                leftBackPower *= robot_speed;
+                rightBackPower *= robot_speed;
+                LEFT_DRIVEAsDcMotor.setPower(leftBackPower);
+                RIGHT_DRIVEAsDcMotor.setPower(rightBackPower);
             }
         }
     }
 }
-
