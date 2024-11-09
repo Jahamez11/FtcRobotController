d1de72e17480f6813e8fb44fccec0fbb14b80fff
Jahamez11 <145066370+Jahamez11@users.noreply.github.com>
2024-11-09 09:54:04 -0800

    Update fresh for 2024

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TT_Auto.java
@@ -0,0 +1,58 @@
+
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+public class TT_Auto extends LinearOpMode {
+    private DcMotor FRONTLEFT_DRIVE = null;
+
+    private DcMotor FRONTRIGHT_DRIVE = null;
+
+    private DcMotor BACKLEFT_DRIVE = null;
+
+    private DcMotor BACKRIGHT_DRIVE = null;
+    private com.qualcomm.robotcore.hardware.DcMotor ARM_ROTATION = null;
+    private DcMotor ARM_EXTENSION = null;
+    private CRServo LEFT_GRABBER = null;
+    private CRServo RIGHT_GRABBER = null;
+
+    @Override
+    public void runOpMode() {
+
+            FRONTLEFT_DRIVE = hardwareMap.get(DcMotor.class, "FRONTLEFT_DRIVE");
+
+            FRONTRIGHT_DRIVE = hardwareMap.get(DcMotor.class, "FRONTRIGHT_DRIVE");
+
+            BACKLEFT_DRIVE = hardwareMap.get(DcMotor.class, "BACKLEFT_DRIVE");
+
+            BACKRIGHT_DRIVE = hardwareMap.get(DcMotor.class, "BACKRIGHT_DRIVE");
+
+            ARM_EXTENSION = hardwareMap.get(DcMotor.class, "ARMEXTENSION");
+            ARM_ROTATION = hardwareMap.get(DcMotor.class, "ARMROTATION");
+            LEFT_GRABBER = hardwareMap.get(CRServo.class, "LEFT_GRABBER");
+            RIGHT_GRABBER = hardwareMap.get(CRServo.class, "RIGHT_GRABBER");
+
+
+            ARM_EXTENSION.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            ARM_EXTENSION.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            ARM_ROTATION.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            ARM_ROTATION.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            FRONTLEFT_DRIVE.setDirection(DcMotor.Direction.FORWARD);
+            BACKLEFT_DRIVE.setDirection(DcMotor.Direction.REVERSE);
+            FRONTRIGHT_DRIVE.setDirection(DcMotor.Direction.FORWARD);
+            BACKRIGHT_DRIVE.setDirection(DcMotor.Direction.FORWARD);
+            // Put initialization blocks here
+            waitForStart();
+            // Put run blocks here
+            while (opModeIsActive()) {
+                // Put loop blocks here
+                FRONTLEFT_DRIVE.setPower(-1);
+                BACKLEFT_DRIVE.setPower(-1);
+                FRONTRIGHT_DRIVE.setPower(1);
+                BACKRIGHT_DRIVE.setPower(-1);
+                sleep(3000);
+                break;
+            }
+        }
+    }
